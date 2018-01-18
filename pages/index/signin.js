697282b3eaab103a80bddd2ca7cf1da0e538e910
yangxj <yangxuanjiang@hotmail.com>
2018-01-18 13:46:09 +0800

    时间修改

--- pages/index/signin.js
+++ pages/index/signin.js
@@ -10,7 +10,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    END_TIME: parseInt(+new Date("2018/01/18,17:30:00")),//活动结束时间
+    END_TIME: parseInt(+new Date("2018/01/25,17:30:00")),//活动结束时间
     currentTime: '000000', //倒计时时间
     isBegin: false, //倒计时是否结束
     inputValue: [],//身份ID
